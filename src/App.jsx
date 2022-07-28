fcbbdd0f865da0a6cf5ae45c62bb0f0aad81af19
Elu Gonzalez <elugon10@gmail.com>
2022-07-28 19:43:59 +0200

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
+import { set } from 'mongoose';
 import React, { useState } from 'react';
 import './App.css';
 import productData from './products.json';
 
 function App() {
   const [products, setProducts] = useState(productData);
+  
+  const handleDelete= (id) =>{
+
+  const filtered=products.filter(ele=>ele._id!==id)
+  setProducts(filtered)
+  }
+
+  const handleSearch= (e) =>{
+
+    if(e.target.value === ''){
+      setProducts(productData)
+    } else{
+    const filtered=products.filter(ele=>ele.name.toLowerCase().includes(e.target.value.toLowerCase()))
+    setProducts(filtered)
+    }}
 
   return (
     <div className="cart">
       <h1>My shopping cart</h1>
+      <input type="text" placeholder='Search' onChange={(e)=>handleSearch(e)}></input>
 
-      {/* Your code goes here */}
+      {products.map(ele=>{return (<div key={ele._id}>
+      <p>{ele.name}</p>
+      <button onClick={()=>handleDelete(ele._id)}>Delete</button>
+      </div>)} )}
 
     </div>
   );
